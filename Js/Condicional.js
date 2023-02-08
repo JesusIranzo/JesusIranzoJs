5a14f9f6aa0c30ed57f0d32438ede2a597ef99fe
Jesus Iranzo <iranzo.quintana@gmail.com>
2023-02-07 21:19:03 -0500

    Calculadora

--- Js/Condicional.js
+++ Js/Condicional.js
@@ -7,13 +7,3 @@ if((nombreIngresado != "") && (apellidoIngresado !="")){
 } else {
      alert("Error: Ingresa nombre y apellido");
  }
-
-let usuario = prompt ("Ingresa tu Usuario:");
- let password = prompt ("Ingresa tu Contraseña");
- let huellaDactilar = true
-
- if (usuario === "NickyWolf18" && password === "1818051301"){
-     alert("Bienvenido " + usuario)
- } else {
-    console.warn ("No se reconoce usuario y/o contraseña.")
- }
--- Js/Simulador.js
+++ Js/Simulador.js
@@ -1,6 +1,9 @@
-function sumar(nro1, nro2){
-    console.log("resultado:" , nro1 + nro2)
-}
+let consulta = confirm("¿ Quiéres hacer un cálculo ?")
+ while (consulta){
+    iniciarCalculadora()
+    consulta = confirm("¿ Quiére hacer otro cálculo ?")
+ }
+
 
 function calcular(nro1, nro2, ope){
     switch(ope){
@@ -20,7 +23,7 @@ function iniciarCalculadora (){
     let numero1 = parseFloat (prompt ("Ingresa el primer nro"))
     let numero2 = parseFloat (prompt ("Ingresa el segundo nro"))
     let operador = prompt ("Ingresa el opreador { + - * /}")
-        console.log ("Resultado: ",  calcular (numero1, numero2, operador))
+        alert ("Resultado: " + calcular (numero1, numero2, operador))
 }
 
-iniciarCalculadora ()
+
